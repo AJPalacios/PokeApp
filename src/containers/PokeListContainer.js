5d311f9ada21b3442a9363ab78fdebad4873a707
Adan <ajpalacios.sist@gmail.com>
2020-02-15 14:21:34 -0600

    add first pagination

--- src/containers/PokeListContainer.js
+++ src/containers/PokeListContainer.js
@@ -6,21 +6,51 @@ import AppNav from '../components/AppNav';
 class PokeListContainer extends Component {
 
   state ={
-    pokeData: []
+    pokeData: [],
+    next: "",
+    previous: ""
   }
 
   componentDidMount() {
     axios.get('https://pokeapi.co/api/v2/pokemon')
     .then(res => {
-      const {results} = res.data;
+      const { results, next, previous} = res.data;
 
       this.setState({
-        pokeData: results
+        pokeData: results,
+        next,
+        previous
       })
 
     })
     .catch(error =>{
       console.log(error);
+    })
+  }
+
+  nextPage = () => {
+    const { next } = this.state;
+    axios.get(next)
+    .then(r => {
+      const { results, next, previous} = r.data;
+      this.setState({
+        pokeData: results,
+        next,
+        previous
+      })
+    })
+  }
+
+  previousPage = () => {
+    const { previous } = this.state;
+    axios.get(previous)
+    .then(r => {
+      const { results, next, previous} = r.data;
+      this.setState({
+        pokeData: results,
+        next,
+        previous
+      })
     })
   }
 
@@ -30,6 +60,8 @@ class PokeListContainer extends Component {
     return(
       <>
         <AppNav />
+        <button onClick={this.previousPage}>Anterior</button>
+        <button onClick={this.nextPage}>Siguiente</button>
         <List pokedata={pokeData} />
       </>
     );
